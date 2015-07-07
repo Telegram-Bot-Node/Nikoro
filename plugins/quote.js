2649475e408a021f289e2cb9cdeba6270586c421
cris9696 <bld.cris.96@gmail.com>
2015-07-07 15:59:06 +0200

    Random Quote Plugin

---
+++ plugins/quote.js
@@ -0,0 +1,42 @@
+/*
+Random line from the quote file
+
+!q
+
+*/
+
+var quote = function(){
+
+    quotes = [];
+
+    this.init = function(){
+        var fs = require('fs');
+        fs.readFile("./files/quotes",'utf8', function(err, data) {
+            if(err) {
+                return console.log(err);
+            }
+            quotes = data.toString().split("\n");
+            console.log("\tQUOTE: file loaded");
+        }); 
+    };
+
+    this.doStop = function(){
+
+    };
+
+
+    this.doMessage = function (msg, reply) {
+        var re = /!q/i;
+        var match = re.exec(msg.text);  
+        
+        if(match)
+        {
+            choice = Math.floor(Math.random() * quotes.length);
+
+            reply({type: 'text', text: quotes[choice] });
+        }
+    };
+
+};
+
+module.exports = quote;
