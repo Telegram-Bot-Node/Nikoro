b465572f6bcdbd376cc5a793aaf54b9740c6134f
cris9696 <bld.cris.96@gmail.com>
2015-08-08 10:57:13 +0200

    Added setup.sh for env variables

---
+++ setup.sh
@@ -0,0 +1,13 @@
+# Telegram Token 
+# https://core.telegram.org/bots#botfather
+export TELEGRAM_TOKEN=""
+
+# Youtube & Google Search
+export GOOGLE_API_KEY=""
+
+# Twitter
+export TWITTER_CONSUMER_KEY=""
+export TWITTER_CONSUMER_SECRET=""
+export TWITTER_ACCESS_TOKEN_KEY=""
+export TWITTER_ACCESS_TOKEN_SECRET=""
+
