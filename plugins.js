a85e081b2106c24d98c6d208812188dc8c5c9e38
Phill Farrugia <me@phillfarrugia.com>
2015-08-09 16:52:44 +1000

    plugin manager documentation

--- plugins.js
+++ plugins.js
@@ -1,9 +1,30 @@
 /*
-	TODO: Header Documentation
+	PluginManager
+
+	An abstract class used to require an array of external modules
+	or `plugins` specified by an array of module names. Each plugin
+	is safely loaded, configured and initialized by this class before 
+	being used externally.
+
+	Plugin Lifecycle
+	1. Require plugin module
+	2. Call `check()` method
+	3. Call `init()` method
+	4. Plugin now safely running
 */
 
 function PluginManager() {
 
+	/*
+		Performs all setup necessary to begin running a list of plugins.
+		Includes loading, configuring and initialization.
+
+		@param plugins - An array of plugin names corresponding to file
+		names in the plugins directory.
+
+		@return -  An array of loaded, configured, initialized plugin
+		modules or functions.
+	*/
 	PluginManager.prototype.runPlugins = function(plugins) {
 		var loadedPlugins = PluginManager.prototype.loadPlugins(plugins);
 		console.log(loadedPlugins.length + " Plugins loaded");
@@ -14,6 +35,15 @@ function PluginManager() {
 		return runningPlugins;
 	}
 
+	/*
+		Loads a list of plugins by requiring the necessary module
+		and checking that it is configured correctly.
+
+		@param plugins - An array of plugin names corresponding to file
+		names in the plugins directory.
+
+		@return -  An array of loaded, configured plugin modules or functions.
+	*/
 	PluginManager.prototype.loadPlugins = function(plugins) {
 		var loadedPlugins = [];
 		var loadedPluginNames = [];
@@ -35,6 +65,16 @@ function PluginManager() {
 		return loadedPlugins;
 	}
 
+	/*
+		Initializes plugins that have already been loaded by calling
+		their internal `init` method.
+
+		@param plugins - An array of plugin modules or functons that have been
+		required and configured.
+
+		@return -  An array of loaded, configured and initialized plugin 
+		modules or functions.
+	*/
 	PluginManager.prototype.initializePlugins = function(plugins) {
 		var intializedPlugins = [];
 
@@ -46,7 +86,15 @@ function PluginManager() {
 
 		return intializedPlugins;
 	}
+	
+	/*
+		Loads an individual plugin by requiring the necessary module.
 
+		@param plugins - An array of plugin names corresponding to file
+		names in the plugins directory.
+
+		@return -  An array of loaded, configured plugin modules or functions.
+	*/
 	PluginManager.prototype.loadPlugin = function(plugin) {
 		try {
 			var module = require('./plugins/' + plugin);
@@ -58,6 +106,16 @@ function PluginManager() {
 		}
 	}
 
+	/*
+		Validates an individual plugin by checking that all configuration
+		requirements have been met. A plugin is considering configured correctly
+		if its internal `check` method returns true, or if no such method exists.
+
+		@param plugins - A plugin module or function.
+
+		@return -  `true` if no configuration is needed, or if internal plugin 
+		configuration returns `true`. Otherwise returns `false` and is ignored.
+	*/
 	PluginManager.prototype.validatePlugin = function(plugin) {
 		if (typeof plugin.check == 'function') {
 			if (plugin.check()) {
