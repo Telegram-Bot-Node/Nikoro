e1779280fcad8d0874b992672839c0823f6aada3
Cristian Baldi <bld.cris.96@gmail.com>
2015-08-14 15:22:03 +0200

    Improved functionality of parseCommand, added new params

--- util.js
+++ util.js
@@ -15,66 +15,74 @@ var util = {};
         * Starts with either ! or /
         * which is followed by the command name
         * which is followed by a series of arguments 
-            * separeted by space or by what is passed as the split_by variable 
+            * separeted by space or by what is passed as the splitBy variable 
 
     @param message - The message which you want to extract args from
     
-    @param commandName - Optional. The command or commands that will trigger the match.
-                        defaults to "[a-zA-Z0-9]+" (any text) if not set.
-                    Example: 
-                    message =  "/yahoo text"
-                    parseCommand(message) ->  ["yahoo","text"]
-                    parseCommand(message,"yahoo") ->  ["yahoo","text"]
-                    parseCommand(message,["yahoo","y"]) ->  ["y","text"]
-                    parseCommand(message,"google") ->  null 
-                        since the message command does not match the command passed as parameter
-
+    @param commandName - The command (string) or commands (array) that will trigger the match.            
+            message =  "/yahoo text"
+            parseCommand(message) ->  ["yahoo","text"]
+            parseCommand(message,{commandName: "yahoo"}) ->  ["yahoo","text"]
+            parseCommand(message,{commandName: ["yahoo","y"]}) ->  ["y","text"]
+            parseCommand(message,{commandName: "google"}) ->  null 
+                since the message command does not match the command passed as parameter
     
-    @param split_by - Optional. The character that divides the parameters, 
-                        defaults to " " (space) if not set. 
-
+    @param options - An object that can have different properties specified.
+    
+    @param options.splitBy - Optional. The character that divides the parameters of the command, 
+            Defaults to " " (space) if not set. 
+    
+    @param options.joinParams - Optional. Join all the parameters into a single string parameter, 
+            The parameters will be joined with a " " (space) 
+            Defailts to false if not set.
+            
     @return - An array of the args of the command, 
-        the first element of the array is the command called. (C / Python style)
-        Returns null if the message is not a valid command (does not match the regex)  
+        The first element of the array is the command name. (C / Python style)
+        Returns null if the message is not a valid command (does not match the specified commands)  
 */
 
-util.parseCommand = function(message, commandName, split_by){
-    if (!split_by)
-        split_by = " ";
+util.parseCommand = function(message, commandName, options){
+    
+    var splitBy = options.splitBy || " ";
+    var joinParams = options.joinParams || false;
+
+    if (!splitBy)
+        splitBy = " ";
 
     var regexParam = ""
-    if (!commandName) {
-        commandName = "[a-zA-Z0-9]+";
-    } else {
-        if (typeof commandName === 'string')
-            commandName = [commandName];
+    if (typeof commandName === 'string')
+        commandName = [commandName];
 
+    //let's build a valid regex that matches any of the passed commands
+    //["g","google","ggl"] -> g|google|ggl
+    regexParam = ""; 
+    for (var i = 0; i < commandName.length; i++) {
+        regexParam += commandName[i];
 
-        //let's build a valid regex that matches any of the passed commands
-        //["g","google","ggl"] -> g|google|ggl
-        regexParam = ""; 
-        for (var i = 0; i < commandName.length; i++) {
-            regexParam += commandName[i];
-
-            if (i != commandName.length - 1)
-                regexParam += "|";
-        }
+        if (i != commandName.length - 1)
+            regexParam += "|";
     }
+    
     var re = new RegExp("(?:!|\\/)(" + regexParam +")\\s+(.*)"); 
     var match = re.exec(message + " "); //we have to add this space because we specified "\s+" in the regex, to separate command from params, if we use "\s*" "!google test" -> ["g","oogle","test"] 
         
     var args = [];
     if (match) {
         var command = match[1].trim();
-        var params = match[2].split(split_by);
+        var params = match[2].split(splitBy);
+        
+        if (joinParams)
+            params = [params.join(" ")];
         
         args = [command];
-
+        
         for (var i=0;i<params.length;i++) {
             var param = params[i].trim();
             if(param.length>0)
                 args.push(param);
         }
+
+       
         return args;
 
     } else {
