c9efe4d9ef44a15e69bb1674f61e74e9a00c0ccd
crisbal <bld.cris.96@gmail.com>
2016-01-10 17:35:02 +0100

    Added function to save temp web resources

--- src/Util.js
+++ src/Util.js
@@ -6,6 +6,9 @@
 
 var Util = {};
 
+var request = require('request');
+var crypto = require('crypto');
+var fs = require('fs');
 
 /*
     A bit hacked together but it works.
@@ -115,5 +118,24 @@ Util.escapeRegExp = function(str) {
   return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
 };
 
+
+Util.downloadAndSaveTempResource = function(url, extension, callback) {
+    var current_date = (new Date()).valueOf().toString();
+    var random = Math.random().toString();
+    var fn = "/tmp/ntb-tempres" + crypto.createHash('sha1').update(current_date + random).digest('hex') + "." + extension;
+    
+    //console.log("\tUtil.downloadAndSaveTempResource: " + url)
+    
+    var options = {
+        url: url,
+        headers: {
+            'User-Agent': 'stagefright/1.2 (Linux;Android 5.0)'
+        }
+    };
+
+    request(options).pipe(fs.createWriteStream(fn)).on('close', function(){
+        callback(fn);
+    });
+}
 
 module.exports = Util;
