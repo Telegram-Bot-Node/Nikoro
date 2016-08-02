51afa7d06f565911eeadf92f5ce024c7fbdf0220
Cristian Baldi <bld.cris.96@gmail.com>
2016-08-02 15:40:23 +0200

    Rename LoggerProxy to Logger

--- src/plugins/Logger.js
+++ src/plugins/Logger.js
@@ -1,25 +1,29 @@
 import Plugin from "./../Plugin";
-import Rebridge from "rebridge";
 
-var db = Rebridge();
-
-export default class LoggerProxy extends Plugin {
+export default class Logger extends Plugin {
     get plugin() {
         return {
-            name: "Ignore",
-            description: "Ignore users",
-            help: "Syntax: /ignore <username>",
-            isProxy: true
+            name: "Logger",
+            description: "Log stuff",
+            help: "",
+
+            visibility: this.Visibility.HIDDEN,
+            type: this.Type.PROXY
+
+            needs: {
+                database: true
+            }
         };
     }
 
     proxy(eventName, message) {
         return new Promise(function(resolve, reject) {
             if (message.from.username) {
-                if (!db["chat" + message.chat.id])
-                    db["chat" + message.chat.id] = {};
-                db["chat" + message.chat.id]["ID_" + message.from.username] = message.from.id;
+                if (!this.db["chat" + message.chat.id])
+                    this.db["chat" + message.chat.id] = {};
+                this.db["chat" + message.chat.id][message.from.username] = message.from.id;
             }
+
             // Register people who join or leave, too.
             if (message.new_chat_participant || message.left_chat_participant) {
                 const source = message.new_chat_participant ?
@@ -27,7 +31,7 @@ export default class LoggerProxy extends Plugin {
                     message.left_chat_participant;
                 if (!db["chat" + message.chat.id])
                     db["chat" + message.chat.id] = {};
-                db["chat" + message.chat.id]["ID_" + source.username] = source.id;
+                db["chat" + message.chat.id][source.username] = source.id;
             }
             resolve(message);
         });
