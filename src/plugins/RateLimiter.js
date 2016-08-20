83050d7f5095f2905390604ab8480f73cff3604f
Capacitor Set <CapacitorSet@users.noreply.github.com>
2016-08-20 18:04:19 +0200

    [RateLimiter] Reject messages older than 1 minute

--- src/plugins/RateLimiter.js
+++ src/plugins/RateLimiter.js
@@ -3,6 +3,7 @@ import Util from "./../Util";
 
 var lastMessages = {};
 const spamInterval = 1000;
+const old_threshold = 60; // Reject messages older than this many seconds
 
 export default class RateLimiter extends Plugin {
     get plugin() {
@@ -19,6 +20,9 @@ export default class RateLimiter extends Plugin {
         const now = (new Date()).getTime();
         const author = message.from.id;
         const lastMessage = lastMessages[author];
+
+        if ((Math.round(now / 1000) - message.date) > old_threshold)
+            return Promise.reject();
 
         // The difference is in milliseconds.
         if (lastMessage && ((now - lastMessage) < spamInterval)) {
