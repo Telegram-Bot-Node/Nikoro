83ddcd497ec5589d4ed5007d9e79139a832e175c
Avi Patel <avipatel91@gmail.com>
2016-10-20 23:59:16 -0700

    Adding fixes from code review.

--- src/plugins/UserStats.js
+++ src/plugins/UserStats.js
@@ -1,11 +1,12 @@
 import Plugin from "./../Plugin";
+import Util from "../Util";
 
 export default class UserStats extends Plugin {
     static get plugin() {
         return {
             name: "UserStats",
             description: "Get user stats on message count.",
-            help: "Enter /UserStats to get statistics.",
+            help: "Enter /userstats to get statistics.",
             type: Plugin.Type.PROXY,
             needs: {
                 database: true
@@ -14,41 +15,45 @@ export default class UserStats extends Plugin {
     }
 
     proxy(eventName, message) {
-        if (message.from.username) {
-            const chatId = message.chat.id;
-            const userId = message.from.id;
-            if (!this.db["stat" + chatId]) {
-                this.db["stat" + chatId] = {};
-                this.db["stat" + chatId].totalMessageCount = 0;
-            }
-            if (!this.db["stat" + chatId][userId]) {
-                this.db["stat" + chatId][userId] = {
-                    id: userId,
-                    username: message.from.username,
-                    messageCount: 0
-                };
-            }
-            this.db["stat" + chatId][userId].messageCount++;
-            this.db["stat" + chatId].totalMessageCount++;
+        if (!message.from.username) return;
+        const chatId = message.chat.id;
+        const userId = message.from.id;
+        if (!this.db["stat" + chatId]) {
+            this.db["stat" + chatId] = {};
+            this.db["stat" + chatId].totalMessageCount = 0;
         }
+        if (!this.db["stat" + chatId][userId]) {
+            this.db["stat" + chatId][userId] = {
+                userId,
+                username: message.from.username,
+                messageCount: 0
+            };
+        }
+        this.db["stat" + chatId][userId].messageCount++;
+        this.db["stat" + chatId].totalMessageCount++;
         return Promise.resolve(message);
     }
 
     onText(message, reply) {
-        if (message.text === '/UserStats') {
-            let text = `Total Messages:\n\n`;
-            const statsObject = this.db["stat" + message.chat.id];
-            const totalCount = statsObject.totalMessageCount;
-            for (let key in statsObject) {
-                if (key !== 'totalMessageCount') {
-                    const username = statsObject[key].username;
-                    const id = statsObject[key].id;
-                    const count = statsObject[key].messageCount;
-                    const percentage = (count / totalCount * 100).toFixed(4);
-                    text += `${username} [${id}]: ${count} (${percentage}%)\n`;
-                }
+        const parts = Util.parseCommand(message.text, "userstats");
+        if (!parts) return;
+        let text = `Total Messages:\n\n`;
+        const statsObject = this.db["stat" + message.chat.id];
+        const totalCount = statsObject.totalMessageCount;
+        const userList = Object.keys(statsObject)
+            .map(item => {
+                return statsObject[item];
+            }).filter(item => {
+                return typeof item === "object";
+            }).sort((a, b) => {
+                return b.messageCount - a.messageCount;
+            });
+        for (let user of userList) {
+            if (user) {
+                const percentage = (user.messageCount / totalCount * 100).toFixed(4);
+                text += `${user.username} [${user.userId}]: ${user.messageCount} (${percentage}%)\n`;
             }
-            reply({type: 'text', text: text});
         }
+        reply({type: 'text', text});
     }
 }
