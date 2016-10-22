709fd99b02dd1705e60f178b5a5b3b29d096e25f
Cristian Achille <blackdev1l@autistici.org>
2016-10-22 10:25:36 +0000

    Add Roll Plugin
    This is a simple plugin,
    command is just /roll NdM where N is the number of dice to use and
    M the number of faces the dice have.

--- src/PluginManager.js
+++ src/PluginManager.js
@@ -59,7 +59,10 @@ export default class PluginManager {
                             [
                                 "enableplugin",
                                 "disableplugin"
-                            ]
+                            ],
+                            {
+                                overrideDeprecation: true
+                            }
                         );
                         if (!parts) return message;
                         if (!Auth.isAdmin(message.from.id)) return message;
@@ -87,18 +90,14 @@ export default class PluginManager {
                         }
                         return message;
                     })
-                    .then(message => Promise.all(
-                        this.plugins
-                            .filter(plugin => (plugin.plugin.type & Plugin.Type.PROXY) === Plugin.Type.PROXY)
-                            .map(plugin => plugin.proxy(eventName, message))
-                    ))
-                    // Convert the array of messages to a single message
-                    .then(array => {
-                        if (array.length === 0) return message;
-                        const msg = array[0];
-                        assert(array.every(item => item === msg));
-                        return msg;
-                    })
+                    .then(
+                        message => Promise.all(
+                            this.plugins
+                                .filter(plugin => (plugin.plugin.type & Plugin.Type.PROXY) === Plugin.Type.PROXY)
+                                .map(plugin => plugin.proxy(eventName, message))
+                        )
+                        .then(() => message)
+                    )
                     .then(message => this.emit(
                         eventName,
                         message,
