e1bd414e06ba133faf8644e26b10790701085f5b
Capacitor Set <CapacitorSet@users.noreply.github.com>
2016-10-22 22:49:42 +0200

    Add Markov plugin

--- src/plugins/Markov.js
+++ src/plugins/Markov.js
@@ -1,29 +1,39 @@
 import Plugin from "./../Plugin";
-import markov from "blather";
-
-var m = markov();
-const rate = 0;
+import Blather from "blather";
 
 export default class Markov extends Plugin {
 
     static get plugin() {
         return {
             name: "Markov",
-            description: "Generates random text."
+            description: "Generates random text.",
+            needs: {
+                database: true
+            }
         };
     }
 
+    m = null;
+    rate = 0;
+
     start() {
-        
+        if (this.db) {
+            this.m = Blather.destringify(JSON.stringify(this.db));
+        } else {
+            this.m = new Blather();
+        }
     }
 
     onText(message, reply) {
-        if (message.text !== "/m")
-            m.addFragment(message.text);
-        if (Math.random() > rate) return;
+        if (message.text !== "/markov") {
+            this.m.addFragment(message.text);
+            if (Math.random() > this.rate) return;
+        }
         reply({
             type: "text",
-            text: m.generateFragment()
+            text: this.m.generateFragment()
         });
+
+        this.db = JSON.parse(this.m.stringify());
     }
 }
