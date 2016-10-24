451e249fce0ab09a33cf07b5350bff5c3acb90ce
Capacitor Set <CapacitorSet@users.noreply.github.com>
2016-10-24 11:24:04 +0200

    Add 8ball plugin

---
+++ src/plugins/8ball.js
@@ -0,0 +1,42 @@
+import Plugin from "../Plugin";
+
+export default class The8Ball extends Plugin {
+    static get plugin() {
+        return {
+            name: "The8Ball",
+            description: "Magic 8-Ball!",
+            help: "The Magic 8-Ball will give an answer to all your yes-no questions.\n/8ball question",
+        };
+    }
+
+	choices = [
+		"It is certain",
+		"It is decidedly so",
+		"Without a doubt",
+		"Yes definitely",
+		"You may rely on it",
+		"As I see it, yes",
+		"Most likely",
+		"Outlook good",
+		"Yes",
+		"Signs point to yes",
+		"Reply hazy try again",
+		"Ask again later",
+		"Better not tell you now",
+		"Cannot predict now",
+		"Concentrate and ask again",
+		"Don't count on it",
+		"My reply is no",
+		"My sources say no",
+		"Outlook not so good",
+		"Very doubtful"
+	];
+
+    onCommand({message, command, args}, reply) {
+    	if (command !== "8ball") return;
+    	reply({
+    		type: "text",
+    		text: this.choices[Math.floor(Math.random() * this.choices.length)]
+    	});
+    }
+}
