a60cfd13e83b3fb286133b192aa9ffb958df581a
Capacitor Set <CapacitorSet@users.noreply.github.com>
2016-10-24 23:37:40 +0200

    Remove dead code

--- src/plugins/BoobsButts.js
+++ src/plugins/BoobsButts.js
@@ -32,37 +32,4 @@ export default class BoobsButts extends Plugin {
             return;
         }
     }
-
-    static findValidPic(s, reply) {
-        if (s > 50)
-            return;
-
-        reply({type: "status", status: "upload_photo"});
-
-        const url = Imgur.generateUrl(6);
-        const options = {
-            method: 'HEAD',
-            uri: `http://i.imgur.com/${url}.png`
-        };
-
-        request(options, (error, response) => {
-            if (error)
-
-            if (response.statusCode === 404 || response.headers["content-length"] === "503")
-                return Imgur.findValidPic(s + 1, reply);
-
-            reply({
-                type: 'text',
-                text: `https://i.imgur.com/${url}.png`
-            });
-        });
-    }
-
-    static generateUrl(len) {
-        let url = "";
-        const letters = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
-        for (let i = 0; i < len; i++)
-            url += letters[Math.floor(Math.random() * letters.length)];
-        return url;
-    }
 }
