461e93567c7509946e5a940d8f17a6a9bcf22102
Capacitor Set <CapacitorSet@users.noreply.github.com>
2016-10-25 20:23:32 +0200

    Add Set plugin

---
+++ src/plugins/Set.js
@@ -0,0 +1,67 @@
+import Plugin from "./../Plugin";
+import Util from "../Util";
+
+export default class Set extends Plugin {
+
+    static get plugin() {
+        return {
+            name: "Set",
+            description: "Trigger bot responses whenever someone says a specific sentence.",
+            help: "`/set <trigger> <response>` to set a trigger, `/unset <trigger>` to delete it.",
+
+            needs: {
+                database: true
+            }
+        };
+    }
+
+    start() {
+        if (!this.db.replacements)
+            this.db.replacements = [];
+    }
+
+    onText(message, reply) {
+        const chatID = message.chat.id;
+
+        for (const item of this.db.replacements) {
+            if (chatID !== item.chatID) continue;
+            if (!Util.startsWith(message.text, item.trigger)) continue;
+            reply({type: "text", text: item.replacement});
+        }
+    }
+
+    onCommand({message, command, args}, reply) {
+        console.log(message);
+        const chatID = message.chat.id;
+        let trigger;
+        let replacement;
+        switch (command) {
+        case "set":
+            if (args.length < 2)
+                return reply({
+                    type: "text",
+                    text: "Syntax: `/set <trigger> <replacement>`"
+                });
+
+            trigger = args.shift();
+            replacement = args.join(" ");
+            this.db.replacements.push({trigger, replacement, chatID});
+            reply({
+                type: "text",
+                text: "Done."
+            });
+            return;
+        case "unset":
+            trigger = args[0];
+            // Take only replacements with either a different chat id or a different trigger
+            this.db.replacements = this.db.replacements.filter(item => (item.chatID !== chatID) || (item.trigger !== trigger));
+            reply({
+                type: "text",
+                text: "Done."
+            });
+            return;
+        default:
+            return;
+        }
+    }
+}
