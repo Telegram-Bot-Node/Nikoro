797a94a1209337fb6749505fb6e456f8d0c08478
CapacitorSet <capacitorset@gmail.com>
2017-08-16 15:35:20 +0200

    Improve shutdown handling

--- src/Bot.js
+++ src/Bot.js
@@ -3,7 +3,7 @@ const Log = require("./Log");
 const Config = JSON.parse(require("fs").readFileSync("./config.json", "utf8"));
 const PluginManager = require("./PluginManager");
 const Auth = require("./helpers/Auth");
-let auth = new Auth(Config);
+const auth = new Auth(Config);
 const assert = require("assert");
 
 const log = Log.get("Bot", Config);
@@ -25,17 +25,21 @@ log.info("Permissions configured.");
 log.info("The bot is online!");
 
 // If `CTRL+C` is pressed we stop the bot safely.
-process.on("SIGINT", handleShutdown);
+process.on("SIGINT", handleShutdown("SIGINT received"));
 
 // Stop safely in case of `uncaughtException`.
-process.on("uncaughtException", handleShutdown);
+process.on("uncaughtException", handleShutdown("Uncaught exception"));
 
-function handleShutdown() {
-    log.info("The bot is shutting down, stopping safely all the plugins...");
-    pluginManager.stopPlugins().then(function() {
-        log.info("All plugins stopped correctly.");
-        process.exit();
-    });
+function handleShutdown(reason) {
+    return (err) => {
+    	if (err) log.error(err);
+        log.warn("Shutting down, reason: " + reason);
+        log.info("Stopping safely all the plugins...");
+        pluginManager.stopPlugins().then(function() {
+            log.info("All plugins stopped correctly.");
+            process.exit();
+        });
+    };
 }
 
 process.on('unhandledRejection', (reason, p) => {
