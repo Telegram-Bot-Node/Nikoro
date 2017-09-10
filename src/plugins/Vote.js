f458ee9c54a023e0493bf2962db3f4012af970ed
Capacitor Set <capacitorset@gmail.com>
2017-09-11 00:20:10 +0200

    Rewrite Vote.js with the simpler syntax, plus an actual rewrite

--- src/plugins/Vote.js
+++ src/plugins/Vote.js
@@ -17,117 +17,70 @@ module.exports = class Vote extends Plugin {
             this.db.votes = {};
     }
 
-    onCommand({message, command, args}, reply) {
-        const chatId = message.chat.id;
-        switch (command) {
-        case "vote": {
-            if (args.length === 0)
-                return reply({
-                    type: "text",
-                    text: "Syntax: `/vote <option>`"
-                });
-            if (!this.db.votes[chatId])
-                return reply({
-                    type: "text",
-                    text: "There is no vote at this time."
-                });
+    get commands() { return {
+        vote: ({args, message}) => {
+            const chatId = message.chat.id;
+            if (args.length === 0) return "Syntax: `/vote <option>`";
+            if (!this.db.votes[chatId]) return "There is no vote at this time.";
 
             this.db.votes[chatId].results[message.from.username] = args.join(" ");
-
-            reply({
-                type: "text",
-                text: "Your vote has been registered."
-            });
-            return;
-        }
-        case "clearvote": {
-            delete this.db.votes[chatId];
-            reply({
-                type: "text",
-                text: "Question cleared."
-            });
-            return;
-        }
-        case "setvote": {
-            if (args.length === 0)
-                return reply({
-                    type: "text",
-                    text: "Please specify a question."
-                });
-
+            return "Your vote has been registered.";
+        },
+        clearvote: ({message}) => {
+            delete this.db.votes[message.chat.id];
+            return "Question cleared.";
+        },
+        setvote: ({args, message}) => {
+            if (args.length === 0) return "Please specify a question.";
             const question = args.join(" ");
 
             // Note that previous results are automatically removed
-            this.db.votes[chatId] = {
+            this.db.votes[message.chat.id] = {
                 text: question,
                 results: {}
             };
 
-            reply({
-                type: "text",
-                text: "Question set."
-            });
-            return;
-        }
-        case "getvote": {
-            if (!this.db.votes[chatId])
-                return reply({
-                    type: "text",
-                    text: "There is no vote at this time."
-                });
+            return "Question set.";
+        },
+        getvote: ({message}) => {
+            const chatId = message.chat.id;
+            if (!this.db.votes[chatId]) return "There is no vote at this time.";
 
             const poll = this.db.votes[chatId];
             const totalVotes = Object.keys(poll.results).length;
             let response = `Question: ${poll.text}\n` +
                 `Vote count: ${totalVotes}\n\n`;
 
-            // Get an array like [{user: "foo", answer: "bar"}, ...]
-            const arrayOfVotes = [];
+            const uniqueAnswers = new Set();
             for (const user in poll.results) {
                 if (!poll.results.hasOwnProperty(user)) continue;
-                arrayOfVotes.push({
-                    user,
-                    answer: poll.results[user]
-                });
+                uniqueAnswers.add(poll.results[user]);
             }
 
-            // The key is the vote, the content is the array of users who voted.
-            // Eg. {"bar": ["foo"]}
-            const voteCounts = {};
-            for (const vote of arrayOfVotes) {
-                const answer = vote.answer;
-                const user = vote.user;
-                if (voteCounts[answer])
-                    voteCounts[answer].push(user);
-                else
-                    voteCounts[answer] = [user];
+            // Map<Answer, Array<Users>>
+            const answerToUsersMap = new Map();
+            for (const answer of uniqueAnswers) {
+                const users = [];
+                // Which users voted for this answer?
+                for (const user in poll.results) {
+                    if (!poll.results.hasOwnProperty(user)) continue;
+                    if (poll.results[user] !== answer) continue;
+                    users.push(user);
+                }
+                answerToUsersMap.set(answer, users);
             }
 
-            // Make a string for each option, and then sort the options by vote count
-            const voteStrings = [];
-            for (const answer in voteCounts) {
-                if (!voteCounts.hasOwnProperty(answer)) continue;
-                const votes = voteCounts[answer];
-                const percentage = (100 * votes.length / totalVotes).toFixed(2);
-                voteStrings.push({
-                    votes,
-                    string: `${answer}: ${votes.length} votes (${percentage}%)\n` +
-                        votes.join(", ")
-                });
-            }
-            response += voteStrings
-                .sort((a, b) => a.votes - b.votes)
-                .map(tuple => tuple.string)
-                .join("\n\n");
+            // Sort the map (https://stackoverflow.com/a/31159284) by user array length
+            const sortedMap = new Map([...answerToUsersMap.entries()]
+                .sort(([[answerA, usersA], [answerB, usersB]]) => usersA.length - usersB.length));
 
-            reply({
-                type: "text",
-                text: response
+            sortedMap.forEach((users, answer) => {
+                const votes = users.length;
+                const percentage = (100 * votes / totalVotes).toFixed(2);
+                response += `${answer}: ${votes} votes (${percentage}%)\n` + users.join(", ") + "\n";
             });
-            return;
+
+            return response;
         }
-        default:
-            return;
-        }
-    }
+    };}
 };
