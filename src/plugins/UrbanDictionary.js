edddf698cd6c4593d22246db3c30aa5dff06a004
Caley Woods <caley.w@gmail.com>
2017-10-02 07:27:47 -0500

    Adds Urban Dictionary Plugin (#139)
    * Adds Urban Dictionary Plugin
    
    This adds an Urban Dictionary plugin so users can get definitions from
    the Urban Dictionary site via the /ud command to the bot.
    
    * Adds Error Checking And Help Response Cleanup
    
    The ajax request to Urban Dictionary now has a catch statement to
    display an error to the end user.
    
    "Commands" verbiage was removed from the /help text.
    
    * Sends Error To Console
    
    This adds error logging out to the bot console.
    
    A bit of text was also re-added to the `/help` command.
    
    * JSON.parse Returned Error
    
    JSON parse the returned error so it's readable in the console.
    
    * Rework Help Verbiage
    
    Fixes help verbiage a bit.

---
+++ src/plugins/UrbanDictionary.js
@@ -0,0 +1,49 @@
+const Plugin = require('../Plugin');
+const request = require('request-promise');
+
+module.exports = class UrbanDictionary extends Plugin {
+    static get plugin() {
+        return {
+            name: "UrbanDictionary",
+            description: "Fetches the Urban Dictionary definition for a supplied word or phrase.",
+            help: "`/ud <query>` returns the definition for a given word or phrase"
+        };
+    }
+
+    get commands() {
+        return {
+            ud: ({args, message}) => {
+                if (args.length === 0) {
+                    this.sendMessage(message.chat.id, "Please provide a search term after /ud");
+                    return;
+                }
+
+                const query = args.join(' ');
+
+                request(`http://api.urbandictionary.com/v0/define?term=${encodeURIComponent(query)}`)
+                    .then(response => {
+                        const data = JSON.parse(response);
+                        /* data.list will be an array, it seems like typically
+                           the most popular defintion is at the zero index though.
+                        */
+                        const def = data.list[0];
+                        const opts = {parse_mode: 'Markdown'};
+                        let msg;
+
+                        if (data.result_type === 'no_results') {
+                            msg = `Sorry, I was unable to find results for: ${args.join(' ')}.`;
+                        } else {
+                            msg =
+                                `*${def.word}*: ${def.definition}\n\n_${def.example}_`;
+                        }
+
+                        this.sendMessage(message.chat.id, msg, opts);
+                    })
+                    .catch(err => {
+                        this.sendMessage(message.chat.id, 'An error occured.');
+                        this.log.error(`An error occured. The returned error was: ${JSON.parse(err)}`);
+                    });
+            }
+        };
+    }
+};
