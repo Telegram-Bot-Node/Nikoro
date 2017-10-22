4eee393a5faa51dca097342073a6e30833345ee4
CapacitorSet <capacitorset@gmail.com>
2017-10-22 21:33:05 +0200

    Rewrite Auth plugin

--- src/plugins/Auth.js
+++ src/plugins/Auth.js
@@ -1,11 +1,17 @@
 const Plugin = require("./../Plugin");
+const Util = require("./../Util");
+
 module.exports = class AuthPlugin extends Plugin {
 
     static get plugin() {
         return {
             name: "Auth",
             description: "Plugin to handle authentication",
-            help: ""
+            help: `Commands:
+
+/modlist, /adminlist to list mods and admins respectively
+/addmod, /delmod to add or remove mods
+/addadmin, /deladmin to add or remove admins`
         };
     }
 
@@ -15,43 +21,47 @@ module.exports = class AuthPlugin extends Plugin {
         this.auth = obj.auth;
     }
 
-    onCommand({message, command, args}) {
-        const author = message.from.id;
-        const chat = message.chat.id;
-        const targetId = args[0];
+    get commands() {
+        return {
+            modlist: ({message}) => JSON.stringify(this.auth.getMods(message.chat.id)),
+            adminlist: ({message}) => JSON.stringify(this.auth.getAdmins(message.chat.id)),
+            addmod: ({message, args}) => this.doAction("mod", "addMod", message, args),
+            delmod: ({message, args}) => this.doAction("mod", "removeMod", message, args),
+            addadmin: ({message, args}) => this.doAction("admin", "addAdmin", message, args),
+            deladmin: ({message, args}) => this.doAction("admin", "removeAdmin", message, args)
+        };
+    }
 
-        switch (command) {
-        case "modlist":
-            return this.sendMessage(message.chat.id, JSON.stringify(this.auth.getMods(chat)));
-        case "adminlist":
-            return this.sendMessage(message.chat.id, JSON.stringify(this.auth.getAdmins(chat)));
-        // The code from here on is admin-only.
-        case "addmod":
-            if (!this.auth.isAdmin(author, chat)) return;
-
-            this.auth.addMod(targetId, chat);
-
-            return this.sendMessage(message.chat.id, "Done.");
-        case "addadmin":
-            if (!this.auth.isAdmin(author, chat)) return;
-
-            this.auth.addAdmin(targetId, chat);
-
-            return this.sendMessage(message.chat.id, "Done.");
-        case "delmod":
-            if (!this.auth.isAdmin(author, chat)) return;
-
-            this.auth.removeMod(targetId, chat);
-
-            return this.sendMessage(message.chat.id, "Done.");
-        case "deladmin":
-            if (!this.auth.isAdmin(author, chat)) return;
-
-            this.auth.removeAdmin(targetId, chat);
-
-            return this.sendMessage(message.chat.id, "Done.");
+    doAction(privilege, command, {from, chat}, args) {
+        switch (privilege) {
+        case "mod":
+            if (!this.auth.isMod(from.id, chat.id))
+                return "Insufficient privileges.";
+            break;
+        case "admin":
+            if (!this.auth.isAdmin(from.id, chat.id))
+                return "Insufficient privileges.";
+            break;
         default:
-            return;
+            return "Unknown privilege: " + privilege;
         }
+
+        if (args.length !== 1)
+            return "Please supply one ID/username.";
+        let target;
+        if (Number(args[0]))
+            target = Number(args[0]);
+        else {
+            try {
+                target = Util.nameResolver.getUserIDFromUsername(args[0]);
+            } catch (e) {
+                return "Couldn't resolve username. Did you /enable UserInfo?";
+            }
+        }
+        if (!target)
+            return "Invalid user (couldn't parse ID, or unknown username).";
+
+        this.auth[command](target, chat.id);
+        return "Done.";
     }
 };
