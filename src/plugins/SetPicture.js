fe51344d06eb50e26d8f2285f937704093acccdc
CapacitorSet <capacitorset@gmail.com>
2017-12-11 18:58:56 +0100

    Add /setpicture in reply

--- src/plugins/SetPicture.js
+++ src/plugins/SetPicture.js
@@ -18,22 +18,32 @@ module.exports = class SetPicture extends Plugin {
 
     get commands() {
         return {
-            setpicture: () => "Send a picture with the caption /setpicture to set the chat's picture."
+            setpicture: ({message}) => {
+                if (!message.reply_to_message || !message.reply_to_message.photo)
+                    return "Reply to a picture with the caption /setpicture to set the chat's picture.";
+                if (!this.auth.isMod(message.from.id, message.chat.id))
+                    return "Insufficient privileges.";
+                this.setPhoto(message.reply_to_message);
+            }
         };
+    }
+
+    setPhoto(message) {
+        const fileId = message.photo[message.photo.length - 1].file_id;
+        this.downloadFile(fileId, os.tmpdir())
+            .then(path => this.setChatPhoto(message.chat.id, path))
+            .catch(err => this.sendMessage(message.chat.id, "An error occurred:\n\n" + err));
     }
 
     onPhoto({message}) {
         if (!message.caption)
             return;
-        if (message.caption !== "/setpicture")
+        if (!message.caption.startsWith("/setpicture"))
             return;
         if (!this.auth.isMod(message.from.id, message.chat.id)) {
             this.sendMessage(message.chat.id, "Insufficient privileges.");
             return;
         }
-        const fileId = message.photo[message.photo.length - 1].file_id;
-        this.downloadFile(fileId, os.tmpdir())
-            .then(path => this.setChatPhoto(message.chat.id, path))
-            .catch(err => this.sendMessage(message.chat.id, "An error occurred:\n\n" + err));
+        this.setPhoto(message);
     }
 };
